444cff3970b28d652b03176b875804fb8966b3f0
foxstacks <198858136+foxstacks@users.noreply.github.com>
2025-06-15 22:51:25 -0500

    .

--- models/mongo.js
+++ models/mongo.js
@@ -1,7 +1,8 @@
-const mongoose = require('mongoose')
 require('dotenv').config()
+const mongoose = require('mongoose')
 
-mongoose.connect('process.env.MONGO_URI', {
+
+mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 })
