e81296e0234ccd6a022517975827c2754fcb463f
foxstacks <198858136+foxstacks@users.noreply.github.com>
2025-06-15 19:07:51 -0500

    starting message board

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const app = express()
+
+const PORT = 8080
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(PORT, () => {
+    console.log(`Connected via ${PORT}!`)
+})
---
+++ routers/indexRouter.js
@@ -0,0 +1,5 @@
+const messages = [
+    {text: 'Hello there!', user: 'Jack', added: new Date()},
+    {text: 'Meow!', user: 'Nina', added: new Date()}
+]
+
